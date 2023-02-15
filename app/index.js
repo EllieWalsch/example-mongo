a964ad6205bead665986d77ab5216350ad2675d2
EllieWalsch <elliewalsch4@gmail.com>
2023-02-14 20:04:12 -0600

    Add routes

--- app/index.js
+++ app/index.js
@@ -1,1 +1,27 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import express from "express";
+import controller from "./pet/controller.js";
+
+const PORT = 3000;
+
+const app = express();
+
+// Middleware - Allows us to parse JSON data
+app.use(express.json());
+
+// Creates new data
+app.post("/pet/create", (req, res) => {
+  controller.create(req.body).then((result) => {
+    res.json(result);
+  });
+});
+
+// Get all data
+app.get("/pet/readAll", (_, res) => {
+  controller.readAll().then((result) => {
+    res.json(result);
+  });
+});
+
+app.listen(PORT, () => {
+  console.info(`app listening on port ${PORT}`);
+});
